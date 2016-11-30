751dd8ee7372c9249a8e1e9ba60942451daa20e9
Rick Deckard <xxxblasteroid@gmail.com>
2016-11-30 09:58:41 +0300

    fix

--- include.php
+++ include.php
@@ -5,22 +5,21 @@
  * MIT License
  ******************************************************************************/
 
-defined('B_PROLOG_INCLUDED') and (B_PROLOG_INCLUDED === true) or die();
+namespace Rodzeta\Yandexmetricgoals;
 
-use Bitrix\Main\Loader;
-use Bitrix\Main\EventManager;
-use Bitrix\Main\Config\Option;
+defined("B_PROLOG_INCLUDED") and (B_PROLOG_INCLUDED === true) or die();
+
+use Bitrix\Main\{Loader, EventManager, Config\Option};
 
 EventManager::getInstance()->addEventHandler("main", "OnEpilog", function () {
-	if (CSite::InDir("/bitrix/")) {
+	if (\CSite::InDir("/bitrix/")) {
 		return;
 	}
 
-	global $APPLICATION;
-	$APPLICATION->AddHeadString(Option::get("rodzeta.yandexmetricgoals", "yandex_metrika_code"), true);
-	$APPLICATION->AddHeadString(Option::get("rodzeta.yandexmetricgoals", "google_analytics_code"), true);
+	$GLOBALS["APPLICATION"]->AddHeadString(Option::get("rodzeta.yandexmetricgoals", "yandex_metrika_code"), true);
+	$GLOBALS["APPLICATION"]->AddHeadString(Option::get("rodzeta.yandexmetricgoals", "google_analytics_code"), true);
 
-	if (is_readable($_SERVER["DOCUMENT_ROOT"] . \Rodzeta\Yandexmetricgoals\Utils::CACHE_NAME)) {
-		$APPLICATION->AddHeadScript(\Rodzeta\Yandexmetricgoals\Utils::CACHE_NAME);
+	if (is_readable($_SERVER["DOCUMENT_ROOT"] . Utils::CACHE_NAME)) {
+		$GLOBALS["APPLICATION"]->AddHeadScript(Utils::CACHE_NAME);
 	}
 });
